38efdb1586bbf6195ed3989a21cac2403a9816ab
Pawel Daniluk <pawel.daniluk@kotalbert.net>
2024-02-04 17:52:39 +0100

    Feature/commands parsing (#1)
    * Add input parsing
    * Implement minimal CLI

--- main.go
+++ main.go
@@ -1,5 +1,41 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
 func main() {
-	// 	write your code here
+	for {
+		run()
+	}
+}
+
+func run() {
+	fmt.Print("Enter a command and data:")
+	userInput := getInput()
+	command, data := parseInput(userInput)
+
+	switch command {
+	case "exit":
+		fmt.Print("[Info] Bye!\n")
+		os.Exit(0)
+	default:
+		fmt.Print(command, data)
+	}
+	fmt.Println(command, data)
+}
+
+func parseInput(input string) (string, string) {
+	words := strings.Split(input, " ")
+	return words[0], strings.Join(words[1:], " ")
+}
+
+// getInput gets the input from the user, which is a single line of text
+func getInput() string {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	return scanner.Text()
 }
