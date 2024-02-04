9d88d602ec48ca49b21da354b6b75ff16f87d42c
Paweł Daniluk <pawel.daniluk@kotalbert.net>
2024-02-04 17:21:47 +0100

    Add input parsing

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
 func main() {
-	// 	write your code here
+	for {
+		run()
+	}
+}
+
+func run() {
+	fmt.Print("Enter command and data: ")
+	userInput := getInput()
+	command, data := parseInput(userInput)
+	fmt.Println(command, data)
+}
+
+func parseInput(input string) (string, string) {
+	words := strings.Split(input, " ")
+	return words[0], strings.Join(words[1:], " ")
+}
+
+// getInput gets the input from the user, which is a single line of text
+func getInput() string {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	return scanner.Text()
 }
