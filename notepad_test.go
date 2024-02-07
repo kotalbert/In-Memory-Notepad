8879986db54160cc16ce6dbc83a1a689b97fceae
Paweł Daniluk <pawel.daniluk@kotalbert.net>
2024-02-07 21:22:24 +0100

    Move unit test

--- notepad_test.go
+++ notepad_test.go
@@ -5,6 +5,14 @@ import (
 	"testing"
 )
 
+func TestNote_ToString(t *testing.T) {
+	note := Note{Id: 1, Text: "test"}
+	if note.ToString() != "[Info] 1: test" {
+		t.Error("Note.ToString() did not return the expected value")
+	}
+
+}
+
 func TestNotepadConstructor(t *testing.T) {
 	notepad := NewNotepad()
 	if notepad == nil {
